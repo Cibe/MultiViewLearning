37b4848b9ee40fc4623ae5f8c16b3b2133003250
Cibe <k.cibehariharan@gmail.com>
2014-03-22 18:28:45 +0530

    Renamed the variable cotraining

--- driver.m
+++ driver.m
@@ -3,33 +3,33 @@ parametersFile = fopen('parameters1');   % check the sample parameters file for 
 numSplits = fscanf(parametersFile,'%d'); 
 dataPath = fgetl(parametersFile); 
 folderPath = fgetl(parametersFile);
-Cotraining.numberofViews = fscanf(parametersFile,'%d');            
+multiViewlearning.numberofViews = fscanf(parametersFile,'%d');            
 path = fgetl(parametersFile);
 labels = load(strcat(folderPath,path));
-Cotraining.classLabels = labels.labels
-Cotraining.numberLabels = size(Cotraining.classLabels,2);                             
-Cotraining.numberIds = size(Cotraining.classLabels,1)               
+multiViewlearning.classLabels = labels.labels
+multiViewlearning.numberLabels = size(multiViewlearning.classLabels,2);                             
+multiViewlearning.numberIds = size(multiViewlearning.classLabels,1)               
 trainingProportion = 0.1;   
-for view_id=1:Cotraining.numberofViews        
+for view_id=1:multiViewlearning.numberofViews        
     path = fgetl(parametersFile)
     data=load(strcat(folderPath,path));
-    Cotraining.view(view_id).viewdata = data.('view');
-    Cotraining.view(view_id).numfeatures=size(Cotraining.view(view_id).viewdata,2);           % get the data for the
+    multiViewlearning.view(view_id).viewdata = data.('view');
+    multiViewlearning.view(view_id).numfeatures=size(multiViewlearning.view(view_id).viewdata,2);           % get the data for the
 end    
-Cotraining.numberLabels=max(labels.labels);
+multiViewlearning.numberLabels=max(labels.labels);
 numSplits=1;
 %   create training and test set
 % get the accuracys and create a magnifying vector on every split
 
-Cotraining.numberLabels
+multiViewlearning.numberLabels
 
-for j=1:Cotraining.numberLabels
+for j=1:multiViewlearning.numberLabels
     for i=1:numSplits
      load(strcat(dataPath,int2str(i)));
-     Cotraining.initiallabeledVector=tv;
-     Cotraining.accuracy=cotrain(Cotraining.numberofViews,Cotraining.view,Cotraining.initiallabeledVector,Cotraining.classLabels(:,j))
+     multiViewlearning.initiallabeledVector=tv;
+     multiViewlearning.accuracy=cotrain(multiViewlearning.numberofViews,multiViewlearning.view,multiViewlearning.initiallabeledVector,multiViewlearning.classLabels(:,j))
     end
 end
-Accsum = sum(Cotraining.accuracy);
+Accsum = sum(multiViewlearning.accuracy);
 Accuracy = Accsum./numSplits;
 save('Accuracysfile','Accuracy');               
